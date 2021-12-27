641b08da6235207991ccf4c20af67dec730e1b2a
Mitchel the Potato <david.tong.lu@gmail.com>
2021-12-26 17:35:56 -0800

    discovered 3 different ways to solve lanternfish count after 256 days, this one was the one that consumed the least memory

--- day06.py
+++ day06.py
@@ -1,25 +1,28 @@
+
+from collections import Counter
+
+
 with open("day06_input.txt", "r") as f:
     raw_input = [a for a in f.readlines()]
 
-
 lanternfish_internal_timers = [int(a) for a in raw_input[0].split(",")]
-print(lanternfish_internal_timers)
 
-def next_day(timer):
-    if timer == 0:
-        return 6
-    else:
-        return timer - 1
+count = dict(Counter(lanternfish_internal_timers))
+print(count)
 
-def create_new_fish(timers):
-    new_fish = [8 for timer in timers if timer == 0]
-    return new_fish
-
-for day in range(0, 80):
-    new_fish = create_new_fish(lanternfish_internal_timers)
-    lanternfish_internal_timers = [next_day(timer) for timer in lanternfish_internal_timers]
-    lanternfish_internal_timers  = lanternfish_internal_timers + new_fish
-
-print(len(lanternfish_internal_timers))
+def next_day_v2(count):
+    number_of_zeros = count.get(0, 0)
+    new_count = {}
+    for c in range(0, 8):
+        new_count[c] = count.get(c+1, 0)
+    new_count[8] = number_of_zeros
+    new_count[6] += number_of_zeros
+    return new_count
 
 
+for day in range(256):
+    count = next_day_v2(count)
+    print(count)
+
+print(count)
+print(sum(count.values()))
