9c6f573f5c1c517a29296c8ca90f28028e822eda
Mitchel the Potato <david.tong.lu@gmail.com>
2021-12-28 21:50:21 -0800

    day 7 of advent of code 2021 finished

---
+++ day07.py
@@ -0,0 +1,28 @@
+with open("day07_input.txt", "r") as f:
+    raw_input = [a for a in f.readlines()]
+
+crab_arrangement_height = [int(a) for a in raw_input[0].split(",")]
+
+max_height = max(crab_arrangement_height)
+min_height = min(crab_arrangement_height)
+
+fuel_used_for_original_height = dict()
+
+steps_to_fuel = dict()
+steps_to_fuel[0] = 0
+for height in range(1, max_height + 1):
+    steps_to_fuel[height] = steps_to_fuel[height - 1] + height
+
+def calculate_fuel(current_height, target_height):
+    return abs(current_height - target_height)
+
+def calculate_fuel_v2(current_height, target_height):
+    return steps_to_fuel[abs(current_height - target_height)]
+
+
+for height in range(min_height, max_height + 1):
+    all_fuels = [calculate_fuel_v2(a, height) for a in crab_arrangement_height]
+    fuel_used_for_original_height[height] = sum(all_fuels)
+
+print(min(fuel_used_for_original_height.values()))
+
