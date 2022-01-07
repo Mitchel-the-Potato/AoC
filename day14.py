910877fc85eed735568528fa57665436fd56adde
Mitchel the Potato <david.tong.lu@gmail.com>
2022-01-06 19:01:56 -0800

    DIVIDE AND CONQUER MWAHAHAHA used this in the second question of day 14

--- day14.py
+++ day14.py
@@ -1,7 +1,12 @@
+import sys
+sys.setrecursionlimit(3000)
+from functools import lru_cache
+
 from collections import Counter
 
 with open("day14_2_input.txt", "r") as f:
     raw_input = [a for a in f.readlines()]
+inputs = raw_input[0]
 
 
 def convert_rules_line(rule):
@@ -12,7 +17,6 @@ def convert_rules_line(rule):
 with open("day14_input.txt", "r") as f:
     raw_rules = [convert_rules_line(a) for a in f.readlines()]
 
-inputs = raw_input[0]
 
 
 rules = {k:v for k, v in raw_rules}
@@ -31,11 +35,53 @@ def insert_character(s):
             output += v
     return output
 
-for i in range(40):
-    inputs = insert_character("CN")
+#Q2
+def string_to_pairs(ringst):
+    pairs = []
+    for i, c in enumerate(ringst):
+        if i == 0:
+            continue
+        else:
+            pairs.append(ringst[i-1 : i+1])
+    return pairs
 
-print(inputs)
-print(len(inputs))
+@lru_cache(100)
+def expand_pair(pair):
 
-count = Counter(inputs)
-print(count)
+    for i in range(10):
+        output = [pair[0]]
+        for keys in string_to_pairs(pair):
+            output.append(rules[keys])
+        pair = "".join(output)
+    return pair
+
+
+print("BN -> {}".format(expand_pair("BN")))
+
+
+@lru_cache(500)
+def repeat_pair(pair, n):
+    assert n in [10, 20, 30, 40, 50, 60]
+    if n == 10:
+        expand_string = expand_pair(pair)
+        count = Counter(expand_string[1:])
+        return count
+    else:
+        expand_string = expand_pair(pair)
+        count = Counter()
+        for pear in string_to_pairs(expand_string):
+            this_count = repeat_pair(pear, n-10)
+            count = count + this_count
+        return count
+
+
+def repeat_string(s, n):
+    count = Counter()
+    pairs = string_to_pairs(s)
+    for pair in pairs:
+        this_count = repeat_pair(pair, n)
+        count = count + this_count
+    count[s[0]] += 1
+    return count
+
+print(repeat_string(inputs, 40))
